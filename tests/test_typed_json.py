a79ec359b7b4a88598a251f09e0cda4cd3e67c00
Park Sung Min <vamf12@gmail.com>
2020-01-20 21:24:42 +0900

    add load and validate

--- tests/test_typed_json.py
+++ tests/test_typed_json.py
@@ -3,12 +3,10 @@ from pytest import raises
 from typed_json import TypedJson, String
 
 
-class Person(TypedJson):
-    name = String()
-    address = String()
-
-
 def test_class_field_access():
+    class Person(TypedJson):
+        name = String()
+
     # Prevent direct field access, initialized Field instance will cloned on every instance
     with raises(AttributeError):
         assert isinstance(Person.name, String)
@@ -19,7 +17,41 @@ def test_class_field_access():
     assert jane.name is not john.name, "field will copied from original, so must be different."
     assert jane.name.name is john.name.name, "shallow copy will makes attribute values all same"
 
+    john.name.value = 'john'
+    jane.name.value = 'jane'
+    assert john.name.value != jane.name.value, "In different instance values should be different."
+
 
 def test_field_name():
-    assert Person().name.name == 'name'
-    assert Person().address.name == 'address'
+    class Person(TypedJson):
+        name = String()
+        address = String()
+
+    field_name = Person()
+    assert field_name.name.name == 'name'
+    assert field_name.address.name == 'address'
+
+
+def test_load():
+    class Person(TypedJson):
+        name = String()
+        address = String()
+
+    person = Person().load(dict(name='이름'))
+    assert person.name.value == '이름'
+    assert person.address.value is None
+
+
+def test_validate():
+    class Empty(TypedJson):
+        pass
+
+    assert Empty().validate() == dict()
+
+    class Person(TypedJson):
+        name = String()
+        sex = String()
+        address = String(optional=True)
+
+    person = Person().load(dict(name='john'))
+    assert person.validate() == dict(sex=['sex field is required']), "name is filed and address is optional"
--- typed_json/__init__.py
+++ typed_json/__init__.py
@@ -1,30 +1,71 @@
 from copy import copy
-from typing import ClassVar, Dict
+from typing import ClassVar, Dict, Union, TypeVar, Generic, List
+
+ValidJsonType = Union[str]
+
+ErrorType = Union[str]
+ErrorsType = List[ErrorType]
 
 
 class TypedJson:
-    _fields: ClassVar[Dict[str, 'TypedJsonField']]
+    _field_prototypes: ClassVar[Dict[str, 'TypedJsonField']]
+    _fields: Dict[str, 'TypedJsonField']
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
 
-        cls._fields = dict()
+        cls._field_prototypes = dict()
         for name in dir(cls):
             attr = getattr(cls, name)
             if not isinstance(attr, TypedJsonField):
                 continue
             delattr(cls, name)
             attr.name = name
-            cls._fields[name] = attr
+            cls._field_prototypes[name] = attr
 
     def __init__(self):
-        for name, field in self._fields.items():
-            setattr(self, name, copy(field))
+        self._fields = dict()
+        for name, prototype in self._field_prototypes.items():
+            field = copy(prototype)
+            self._fields[name] = field
+            setattr(self, name, field)
+
+    def load(self, source: Dict[str, ValidJsonType]) -> 'TypedJson':
+        for name, value in source.items():
+            field = getattr(self, name, None)
+            if not isinstance(field, TypedJsonField):
+                # ignore other fields
+                continue
+            field.value = value
+        return self
+
+    def validate(self) -> Dict[str, ErrorsType]:
+        errors = {}
+        for field in self._fields.values():
+            field: TypedJsonField
+            field_errors = field.validate()
+            if not field_errors:
+                continue
+            errors[field.name] = field_errors
+        return errors
 
 
-class TypedJsonField:
+T = TypeVar('T')
+
+
+class TypedJsonField(Generic[T]):
     name: str
+    value: T = None
+    optional: bool
+
+    def __init__(self, optional: bool = False):
+        self.optional = optional
+
+    def validate(self) -> ErrorsType:
+        if self.value is not None or self.optional:
+            return []
+        return [f'{self.name} field is required']
 
 
-class String(TypedJsonField):
+class String(TypedJsonField[str]):
     pass
