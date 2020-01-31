ad9b09756afdb26b31905498a3bddc32a262634c
Park Sung Min <vamf12@gmail.com>
2020-01-31 10:22:43 +0900

    add dump

--- tests/test_typed_json.py
+++ tests/test_typed_json.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from typed_json import load
+from typed_json import load, dump
 
 
 def test_integration():
@@ -21,5 +21,14 @@ def test_load():
         age: int
 
     person = load(dict(name='john', age=18), Person)
-    assert person.name == 'john'
-    assert person.age == 18
+    assert person == Person(name='john', age=18)
+
+
+def test_dump():
+    @dataclass
+    class Person:
+        name: str
+        age: int
+
+    person = dump(Person(name='john', age=18))
+    assert person == {'name': 'john', 'age': 18}
--- typed_json/__init__.py
+++ typed_json/__init__.py
@@ -1,8 +1,18 @@
-from typing import Dict, Union, TypeVar, Type
+from typing import Dict, Union, TypeVar, Type, get_type_hints
 
 T = TypeVar('T')
+JsonType = Union[str, 'JsonObjectType']
+JsonObjectType = Dict[str, JsonType]
 
 
-def load(source: Dict[str, Union[str]], target: Type[T]) -> T:
+def load(source: JsonObjectType, target: Type[T]) -> T:
     obj = target(**source)
     return obj
+
+
+def dump(source: T) -> JsonObjectType:
+    json = {}
+    hints = get_type_hints(source.__class__)
+    for name, hint in hints.items():
+        json[name] = getattr(source, name)
+    return json
