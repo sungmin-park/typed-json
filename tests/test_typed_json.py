ee9378dc4b0fbeb4bf9d4773a62e5899c4a75615
Park Sung Min <vamf12@gmail.com>
2020-01-31 10:27:30 +0900

    add spinal case support

--- tests/test_typed_json.py
+++ tests/test_typed_json.py
@@ -3,32 +3,23 @@ from dataclasses import dataclass
 from typed_json import load, dump
 
 
-def test_integration():
-    @dataclass
-    class Person:
-        name: str
-        age: int
-
-    person = load(dict(name='john', age=18), Person)
-    assert person.name == 'john'
-    assert person.age == 18
-
-
 def test_load():
     @dataclass
     class Person:
-        name: str
+        first_name: str
+        last_name: str
         age: int
 
-    person = load(dict(name='john', age=18), Person)
-    assert person == Person(name='john', age=18)
+    person = load({'first-name': 'John', 'last-name': 'Doe', 'age': 18}, Person)
+    assert person == Person(first_name='John', last_name='Doe', age=18)
 
 
 def test_dump():
     @dataclass
     class Person:
-        name: str
+        first_name: str
+        last_name: str
         age: int
 
-    person = dump(Person(name='john', age=18))
-    assert person == {'name': 'john', 'age': 18}
+    person = dump(Person(first_name='John', last_name='Doe', age=18))
+    assert person == {'first-name': 'John', 'last-name': 'Doe', 'age': 18}
--- typed_json/__init__.py
+++ typed_json/__init__.py
@@ -1,12 +1,17 @@
 from typing import Dict, Union, TypeVar, Type, get_type_hints
 
+from stringcase import snakecase, spinalcase
+
 T = TypeVar('T')
 JsonType = Union[str, 'JsonObjectType']
 JsonObjectType = Dict[str, JsonType]
 
 
 def load(source: JsonObjectType, target: Type[T]) -> T:
-    obj = target(**source)
+    kwargs = {}
+    for name, value in source.items():
+        kwargs[snakecase(name)] = value
+    obj = target(**kwargs)
     return obj
 
 
@@ -14,5 +19,5 @@ def dump(source: T) -> JsonObjectType:
     json = {}
     hints = get_type_hints(source.__class__)
     for name, hint in hints.items():
-        json[name] = getattr(source, name)
+        json[spinalcase(name)] = getattr(source, name)
     return json
