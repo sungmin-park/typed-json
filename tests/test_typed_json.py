093a6306b569b54dd7e2675956580d28834468a9
Park Sung Min <vamf12@gmail.com>
2020-01-31 14:02:23 +0900

    add initial validation

--- tests/test_typed_json.py
+++ tests/test_typed_json.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 from typed_json import load, dump
 
@@ -8,6 +9,7 @@ class Person:
     first_name: str
     last_name: str
     age: int
+    sex: Optional[str]
     address: 'Address'
 
 
@@ -39,7 +41,7 @@ class Update(Create):
 
 def test_load():
     # test property load
-    person = load(
+    person, errors = load(
         {
             'first-name': 'John', 'last-name': 'Doe', 'age': 18,
             'address': {
@@ -48,25 +50,58 @@ def test_load():
         },
         Person
     )
-    assert person == Person(first_name='John', last_name='Doe', age=18, address=Address(street='11 st'))
+    assert person == Person(first_name='John', last_name='Doe', age=18, sex=None, address=Address(street='11 st'))
+    assert errors == {}
 
     # test inheritance
-    request = load(
+    request, errors = load(
         {'action': {'__name__': 'Create', '__module__': 'tests.test_typed_json', 'name': 'initial'}},
         Request
     )
     assert request.action == Create(name='initial')
-    request = load(
+    assert errors == {}
+    request, errors = load(
         {'action': {'__name__': 'Update', '__module__': 'tests.test_typed_json', 'id': 1, 'name': 'update'}},
         Request
     )
     assert request.action == Update(id=1, name='update')
+    assert errors == {}
+
+
+def test_validate_required():
+    person, errors = load({}, Person)
+    assert person.first_name is None
+    assert errors == {
+        'first-name': ['first-name is required'],
+        'last-name': ['last-name is required'],
+        'age': ['age is required'],
+        'address': ['address is required'],
+    }
+
+
+def test_class_validation():
+    request, errors = load({'action': 1}, Request)
+    assert request.action is None
+    assert errors == {'action': ['action should be a dict']}
+
+    request, errors = load({'action': {'name': 'invalid'}}, Request)
+    assert request.action is None
+    assert errors == {'action': ['action missing typed-json type information']}
+
+    request, errors = load({'action': {'__name__': 'backdoor', '__module__': 'danger'}}, Request)
+    assert request.action is None
+    assert errors == {'action': ['danger module is not imported']}
+
+    request, errors = load({'action': {'__name__': 'test_class_validation', '__module__': 'tests.test_typed_json'}},
+                           Request)
+    assert request.action is None
+    assert errors == {'action': ['test_class_validation is not dataclass']}
 
 
 def test_dump():
-    person = dump(Person(first_name='John', last_name='Doe', age=18, address=Address(street='11 st')))
+    person = dump(Person(first_name='John', last_name='Doe', age=18, sex=None, address=Address(street='11 st')))
     assert person == {
-        'first-name': 'John', 'last-name': 'Doe', 'age': 18,
+        'first-name': 'John', 'last-name': 'Doe', 'age': 18, 'sex': None,
         'address': {
             '__name__': 'Address',
             '__module__': 'tests.test_typed_json',
--- typed_json/__init__.py
+++ typed_json/__init__.py
@@ -1,32 +1,57 @@
 import sys
+from collections import defaultdict
 from dataclasses import is_dataclass
-from typing import Dict, Union, TypeVar, Type, get_type_hints, Any
+from typing import Dict, Union, TypeVar, Type, get_type_hints, Tuple, List, DefaultDict
 
 from stringcase import spinalcase
 
 T = TypeVar('T')
 JsonObjectType = Dict[str, 'JsonType']
-JsonType = Union[str, Dict[str, Any]]
+JsonType = Union[str, int, Dict[str, JsonObjectType]]
+
+Errors = DefaultDict[str, Union[List[str], 'Errors']]
 
 
-def load(source: JsonObjectType, target: Type[T]) -> T:
+def load(source: JsonObjectType, target: Type[T]) -> Tuple[T, Errors]:
     kwargs = {}
     hints = get_type_hints(target)
 
+    errors = defaultdict(list)
     for name, hint in hints.items():
-        value = source[spinalcase(name)]
+        json_name = spinalcase(name)
+        if json_name not in source:
+            kwargs[name] = None
+            if type(None) not in getattr(hint, '__args__', []):
+                errors[json_name].append(f'{json_name} is required')
+            continue
+
+        value = source[json_name]
         if is_dataclass(hint):
-            cls_name = value['__name__']
-            module_name = value['__module__']
+            if not isinstance(value, dict):
+                kwargs[name] = None
+                errors[name].append(f'{json_name} should be a dict')
+                continue
+            cls_name = value.get('__name__', None)
+            module_name = value.get('__module__', None)
+            if not isinstance(cls_name, str) or not isinstance(module_name, str):
+                kwargs[name] = None
+                errors[name].append(f'{json_name} missing typed-json type information')
+                continue
             # 미리 임포트 되어 있는 module 만 사용할 수 있도록 강제한다.
-            cls = getattr(sys.modules[module_name], cls_name)
+            if module_name not in sys.modules:
+                kwargs[name] = None
+                errors[name].append(f'{module_name} module is not imported')
+                continue
+            cls = getattr(sys.modules[module_name], cls_name, None)
             if not is_dataclass(cls):
-                raise ValueError('target class should be dataclass')
-            value = load(value, cls)
+                kwargs[name] = None
+                errors[name].append(f'{cls_name} is not dataclass')
+                continue
+            value, value_errors = load(value, cls)
         kwargs[name] = value
 
     obj = target(**kwargs)
-    return obj
+    return obj, errors
 
 
 def dump(source: T) -> JsonObjectType:
