57832a52379ed9db41952189a72cf86bc7d1bc1f
Park Sung Min <vamf12@gmail.com>
2020-01-31 12:10:15 +0900

    add name and module for dataclass

--- tests/test_typed_json.py
+++ tests/test_typed_json.py
@@ -3,33 +3,86 @@ from dataclasses import dataclass
 from typed_json import load, dump
 
 
+@dataclass
+class Person:
+    first_name: str
+    last_name: str
+    age: int
+    address: 'Address'
+
+
+@dataclass
+class Address:
+    street: str
+
+
+@dataclass
+class Request:
+    action: 'Action'
+
+
+@dataclass
+class Action:
+    pass
+
+
+@dataclass
+class Create(Action):
+    name: str
+
+
+@dataclass
+class Update(Create):
+    id: int
+    name: str
+
+
 def test_load():
-    @dataclass
-    class Address:
-        pass
+    # test property load
+    person = load(
+        {
+            'first-name': 'John', 'last-name': 'Doe', 'age': 18,
+            'address': {
+                '__name__': 'Address', '__module__': 'tests.test_typed_json', 'street': '11 st'
+            }
+        },
+        Person
+    )
+    assert person == Person(first_name='John', last_name='Doe', age=18, address=Address(street='11 st'))
 
-    @dataclass
-    class Person:
-        first_name: str
-        last_name: str
-        age: int
-        address: Address
-
-    person = load({'first-name': 'John', 'last-name': 'Doe', 'age': 18, 'address': {}}, Person)
-    assert person == Person(first_name='John', last_name='Doe', age=18, address=Address())
+    # test inheritance
+    request = load(
+        {'action': {'__name__': 'Create', '__module__': 'tests.test_typed_json', 'name': 'initial'}},
+        Request
+    )
+    assert request.action == Create(name='initial')
+    request = load(
+        {'action': {'__name__': 'Update', '__module__': 'tests.test_typed_json', 'id': 1, 'name': 'update'}},
+        Request
+    )
+    assert request.action == Update(id=1, name='update')
 
 
 def test_dump():
-    @dataclass
-    class Address:
-        pass
+    person = dump(Person(first_name='John', last_name='Doe', age=18, address=Address(street='11 st')))
+    assert person == {
+        'first-name': 'John', 'last-name': 'Doe', 'age': 18,
+        'address': {
+            '__name__': 'Address',
+            '__module__': 'tests.test_typed_json',
+            'street': '11 st'
+        }
+    }
 
-    @dataclass
-    class Person:
-        first_name: str
-        last_name: str
-        age: int
-        address: Address
-
-    person = dump(Person(first_name='John', last_name='Doe', age=18, address=Address()))
-    assert person == {'first-name': 'John', 'last-name': 'Doe', 'age': 18, 'address': {}}
+    request = dump(Request(action=Create(name='initial')))
+    assert request == {
+        'action': {
+            '__module__': 'tests.test_typed_json', '__name__': 'Create', 'name': 'initial'
+        }
+    }
+    request = dump(Request(action=Update(id=1, name='update')))
+    assert request == {
+        'action': {
+            '__module__': 'tests.test_typed_json', '__name__': 'Update', 'id': 1, 'name': 'update'
+        }
+    }
--- typed_json/__init__.py
+++ typed_json/__init__.py
@@ -1,21 +1,27 @@
+import importlib
 from dataclasses import is_dataclass
-from typing import Dict, Union, TypeVar, Type, get_type_hints
+from typing import Dict, Union, TypeVar, Type, get_type_hints, Any
 
 from stringcase import spinalcase
 
 T = TypeVar('T')
-JsonType = Union[str, 'JsonObjectType']
-JsonObjectType = Dict[str, JsonType]
+JsonObjectType = Dict[str, 'JsonType']
+JsonType = Union[str, Dict[str, Any]]
 
 
 def load(source: JsonObjectType, target: Type[T]) -> T:
     kwargs = {}
     hints = get_type_hints(target)
+
     for name, hint in hints.items():
         value = source[spinalcase(name)]
         if is_dataclass(hint):
-            value = load(value, hint)
+            cls_name = value['__name__']
+            module_name = value['__module__']
+            cls = getattr(importlib.import_module(module_name), cls_name)
+            value = load(value, cls)
         kwargs[name] = value
+
     obj = target(**kwargs)
     return obj
 
@@ -23,9 +29,15 @@ def load(source: JsonObjectType, target: Type[T]) -> T:
 def dump(source: T) -> JsonObjectType:
     json = {}
     hints = get_type_hints(source.__class__)
+
     for name, hint in hints.items():
         value = getattr(source, name)
         if is_dataclass(value):
+            class_name = value.__class__.__name__
+            module_name = value.__class__.__module__
             value = dump(value)
+            value['__name__'] = class_name
+            value['__module__'] = module_name
         json[spinalcase(name)] = value
+
     return json
